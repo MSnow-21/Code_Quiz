854bbee55c06fbdf9ce7abe1af3cb20f280f91f1
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2020-12-14 14:38:20 -0500

    Adding initial variables to logic.js file

---
+++ assets/js/logic.js
@@ -0,0 +1,6 @@
+var timerEl = document.getElementById("time");
+var scoresEl = document.getElementById("scores");
+var startEl = document.getElementById("start");
+var finalscoreEl = document.getElementById("final-score");
+
+
