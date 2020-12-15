9659b96d08a4756839693c85b04f71acb3107269
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2020-12-14 22:50:52 -0500

    Updating html, highscores.html and updated javascript

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,46 @@
 var timerEl = document.getElementById("time");
-var scoresEl = document.getElementById("scores");
-var startEl = document.getElementById("start");
+var startEl = document.getElementById("start-screen");
+var start = document.getElementById("start");
+var questionsEL = document.getElementId ("questions");
+var title = document.getElementById("questions-title");
+var choices = document.getElementById("choices");
+var endscreen = document.getElementById("end-screen");
+var initials = document.getElementById("initials");
+var submitEL = document.getElementById("submit");
 var finalscoreEl = document.getElementById("final-score");
+// call out variable
+
+
+
+
+
+
+
+// create event listener for button
+// activite timer countdown
+// show questions
+    // build question arrays
+        // build a for loop to go through arrays
+        // it needs to create buttons
+        // event listener for each answer button
+        // correct answer add point
+            // how to show extra points in final scoring set
+            // set item/get item local storage.
+        // wrong answer subract extra time
+            // go to next question
+            // keep time going
+            // final button answer - go to score screen
+// score screen
+    // display value of local storage as score
+    // input field for initials
+        // store initials as local storage.
+        // link to high scores page.
+// high scores page
+    // create an element with two local storage items.
+    // append that element to the high scores id
+    // clear high scores to nothing once clear is clicked
+    //
+    
+
 
 
