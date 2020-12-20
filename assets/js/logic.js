5af554a05e7b8df0b8d5d7e990b9f1f826121800
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2020-12-20 16:00:55 -0500

    Updated all notes, added-another timer, getelementbyId property code, arrays for button text content, and event listener for final screen

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -9,66 +9,130 @@ var endscreenEl = document.getElementById("end-screen"); // On end-screen div
 var initialsEl = document.getElementById("initials"); // p tag in end-screen div
 var submitEL = document.getElementById("submit"); // button within end-screen div
 var finalscoreEl = document.getElementById("final-score"); // p tag within end screen div
+var feedbackEl = document.getElementById("feedback"); // div below enter initials screen
 
-// call out variables
-
-// var firstQuestion = ["Commonly used data types DO NOT include:", "nothing here"];
-// var firstAnswer = ["1. strings","2. booleans", "3. alerts", "4. numbers"];
+// Questions
+// "Commonly used data types DO NOT include:", "nothing here"
+var multChoiceOne = ["1. strings","2. booleans", "3. alerts", "4. numbers"];
 // correct answer is "3. alerts".
 
-// var secondQuestion = ["The condition in an if / else statement is enclosed within_____."]
-// var secondAnswer = ["1. qoutes", "2. curly brackets", "3. parentheses", "4. square brackets"];
+// "The condition in an if / else statement is enclosed within_____."
+var multChoiceTwo = ["1. qoutes", "2. curly brackets", "3. parentheses", "4. square brackets"];
 // "3. parentheses"
 
-// var thirdQuestion = ["Arrays in Javascript can be used to store:"]
-// var thirdAnswers = ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"];
+// "Arrays in Javascript can be used to store:"
+var multChoiceThree = ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"];
 // "4. all of the above"
 
-//var fourthQuestion = ["String values must be enclosed within____ when being assigned to variables"]
-// var fourthAnswer = ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"];
+//"String values must be enclosed within____ when being assigned to variables"
+var multChoiceFour = ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"];
 // "3. quotes"
 
-// var fifthQuestion = ["A very useful tool used during development and debugging for printing content to the debugger is:"]
-// var fifthAnswer = ["1. Javascript", "2. terminal / bash", "3. helps", "4. console log"];
+// "A very useful tool used during development and debugging for printing content to the debugger is:"
+var multChoiceFive = ["1. Javascript", "2. terminal / bash", "3. helps", "4. console log"];
 // "4. console log"
 
-// setting local storage
+
+// Setting local storage
 
 localStorage.setItem("score", 0);
 
 // Main timer
 
-timerDigit.textContent = 75;
+var time = 75;
+timerDigit.textContent = time;
 
-startButton.addEventListener("click", function(event){
-    event.preventDefault();
+// seconds = timerDigit.textContent;
 
+// let counter = timerDigit.textContent;
+
+// Timer Style 3 - working timer
+
+// function startCountdown(){
+//     var timeInterval = setInterval(function(){
+//         counter--;
+//         timerDigit.textContent = counter;
+//         console.log(counter);
+
+//         if(counter < 0){
+//             clearInterval(timeInterval);
+//             timerDigit.textContent = "0";
+//             endScreen();
+//         }
+        
+//     }, 1000);
+// }
+
+// Timer Style 4
+
+function startCountdown(){
     var timeInterval = setInterval(function(){
         timerDigit.textContent--;
+        console.log(timerDigit.textContent);
 
-        if(timerDigit.textContent === 0){
-            timerDigit.textContent = "";
+        if(timerDigit.textContent < 1){
+            timerDigit.textContent === 0;
             clearInterval(timeInterval);
+            endScreen();
         }
 
-
     }, 1000);
-    
-    console.log(event);
-});
-// The questions div is hidden in the start screen. Once clicked, this will allow the questions div to show //
-// Process - uses var elements classnames to switch from "hide" to "start" //
+};
+
+
+// Testing timers - since time will not subtract from timerDigit.textContent
+// Timer Style 1
+
+// timerDigit.textContent = 75;
+
+// startButton.addEventListener("click", function(event){
+//     event.preventDefault();
+
+//     var timeInterval = setInterval(function(){
+//         timerDigit.textContent--;
+
+//         if(timerDigit.textContent < 0){
+//             timerDigit.textContent === 0;
+//             clearInterval(timeInterval);
+//         }
+
+
+//     }, 1000);
+// });
+
+
+// Timer style 2
+
+// function startCountdown(seconds){
+//     let counter = seconds;
+
+//     const interval = setInterval(function(){
+//         console.log(counter);
+//         counter--;
+//         timerDigit.textContent = counter;
+
+//         if (counter < 0) {
+//             clearInterval(interval);
+//             timerDigit.textContent = "0";
+//         }
+
+//     }, 1000);
+// }
+
+// Event listener for start button
 
 startButton.addEventListener("click", function(event){
     event.preventDefault();
-    startScreen.className = "hide";
-    questionsEl.className = "start";
+    document.getElementById("start-screen").className = "hide";
+    document.getElementById("questions").className = "start";
     // added local storage start here
     localStorage.setItem("score", "0");
+    // startCountdown(seconds);
+    startCountdown();
 });
 
 // office hours tutoring - worked on overall process and function call outs.
-// added for loop
+// added for-loop
 
 for (var i=1; i<5; i++){
     var button = document.createElement("button");
@@ -77,15 +141,21 @@ for (var i=1; i<5; i++){
     choicesEl.appendChild(button);
 }
 
+// Assigning variables for text content
+
 var button1 = document.getElementById("button1")
 var button2 = document.getElementById("button2")
 var button3 = document.getElementById("button3")
 var button4 = document.getElementById("button4")
 
-button1.textContent = "1. strings";
-button2.textContent = "2. booleans";
-button3.textContent = "3. alerts";
-button4.textContent = "4. numbers";
+button1.textContent = multChoiceOne[0];
+// button1.textContent = "1. strings";
+button2.textContent = multChoiceOne[1];
+// button2.textContent = "2. booleans";
+button3.textContent = multChoiceOne[2];
+// button3.textContent = "3. alerts";
+button4.textContent = multChoiceOne[3];
+// button4.textContent = "4. numbers";
 
 var currentQuestionFunction = questionSetTwo; // see questions below. reassigns variable as program moves through.
 
@@ -102,20 +172,17 @@ document.addEventListener("click", function(e){
 
     }else if (answer === "incorrect"){
         inCorrectAnswer(currentQuestionFunction); // see variable called out on line 82
-        timerDigit.textContent = parseInt(timerDigit.textContent) - 15;
+        timerDigit.textContent = parseInt(timerDigit.textContent) - 15; // 75/5 questions = 15
     }
     
-
 });
 
-// Question One
-// correct answer is 3
+// Question One - variables assigned above - correct answer is 3
 
 startButton.addEventListener("click", function(event){
     event.preventDefault();
 
     titleElement.textContent = "Commonly used data types DO NOT include:"
-    
     button1.setAttribute("data-id", "incorrect")
     button2.setAttribute("data-id", "incorrect")
     button3.setAttribute("data-id", "correct")
@@ -138,10 +205,14 @@ function correctAnswer(nextQuestionFunction){
 
 function questionSetTwo(){
 
-    button1.textContent = "1. quotes";
-    button2.textContent = "2. curley brackets";
-    button3.textContent = "3. parentheses";
-    button4.textContent = "4. square brackets";
+    button1.textContent = multChoiceTwo[0];
+    // button1.textContent = "1. quotes";
+    button2.textContent = multChoiceTwo[1];
+    // button2.textContent = "2. curley brackets";
+    button3.textContent = multChoiceTwo[2];
+    // button3.textContent = "3. parentheses";
+    button4.textContent = multChoiceTwo[3];
+    // button4.textContent = "4. square brackets";
 
     // correct answer is 3
 
@@ -159,10 +230,14 @@ function questionSetThree(){
 
     // correct answer is "4. all of the above"
 
-    button1.textContent = "1. numbers and strings";
-    button2.textContent = "2. other arrays";
-    button3.textContent = "3. booleans";
-    button4.textContent = "4. all of the above";
+    button1.textContent = multChoiceThree[0];
+    // button1.textContent = "1. numbers and strings";
+    button2.textContent = multChoiceThree[1];
+    // button2.textContent = "2. other arrays";
+    button3.textContent = multChoiceThree[2];
+    // button3.textContent = "3. booleans";
+    button4.textContent = multChoiceThree[3];
+    // button4.textContent = "4. all of the above";
 
     titleElement.textContent = "Arrays in Javascript can be used to store:";
     button1.setAttribute("data-id", "incorrect")
@@ -175,10 +250,14 @@ function questionSetThree(){
 
 function questionSetFour(){
 
-    button1.textContent = "1. commas";
-    button2.textContent = "2. curly brackets";
-    button3.textContent = "3. quotes";
-    button4.textContent = "4. parenthesis";
+    button1.textContent = multChoiceFour[0];
+    // button1.textContent = "1. commas";
+    button2.textContent = multChoiceFour[1];
+    // button2.textContent = "2. curly brackets";
+    button3.textContent = multChoiceFour[2];
+    // button3.textContent = "3. quotes";
+    button4.textContent = multChoiceFour[3];
+    // button4.textContent = "4. parenthesis";
 
     // correct answer is 3.
 
@@ -195,10 +274,14 @@ function questionSetFive(){
 
     // correct answer is 4
 
-    button1.textContent = "1. Javascript";
-    button2.textContent = "2. terminal / bash";
-    button3.textContent = "3. helps";
-    button4.textContent = "4. console log";
+    button1.textContent = multChoiceFive[0];
+    // button1.textContent = "1. Javascript";
+    button2.textContent = multChoiceFive[1];
+    // button2.textContent = "2. terminal / bash";
+    button3.textContent = multChoiceFive[2];
+    // button3.textContent = "3. helps";
+    button4.textContent = multChoiceFive[3];
+    // button4.textContent = "4. console log";
 
     titleElement.textContent = "A very useful tool used during development and debugging for printing content to the debugger is:";
     button1.setAttribute("data-id", "incorrect")
@@ -207,6 +290,46 @@ function questionSetFive(){
     button4.setAttribute("data-id", "correct")
 
 }
+
+function endScreen(){
+    document.getElementById("questions").className = "hide";
+    document.getElementById("end-screen").className = "start";
+
+    var finalScore = localStorage.getItem("score");
+    finalscoreEl.textContent = finalScore;
+
+}
+
+// name signing event listener
+
+function displayMessage(message){
+    feedbackEl.textContent = message;
+
+}
+
+submitEL.addEventListener("click", function(event){
+    event.preventDefault();
+
+    var initials = document.querySelector("#initials").value;
+    // var finalscore = document.querySelector("#final-score").value;
+    var lastScore = localStorage.getItem("score");
+
+    if (initials === ""){
+        displayMessage("Error - Please enter 3 initials");
+    }else if (initials < 2){
+        displayMessage("Error - Please enter 3 initials")
+    }else{
+        displayMessage("Success- value entered")
+    }
+
+    localStorage.setItem("initials", initials);
+    localStorage.setItem("finalScore", lastScore);
+
+    feedbackEl.setAttribute("class", "feedback-start")
+
+});
+
+
 
 // Final Score //
 
